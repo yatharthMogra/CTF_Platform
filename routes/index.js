e569d3a731ffbe3d5692c9fe6c064a7c555a5b40
Yashodharm Mogra <yashodharm@Yashodharms-MacBook-Pro.local>
2021-07-04 22:19:42 +0530

    Initialized with app.js

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    console.log('The Platform is under Construction.')
+})
+
+module.exports = router
